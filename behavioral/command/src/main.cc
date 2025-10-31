00f4f22d7754cc6d65b01d88fabf0e5a773e531e
luizf <luizfeliperomero10@gmail.com>
2025-10-31 19:33:23 -0300

    Add Remote to be the invoker

--- behavioral/command/src/main.cc
+++ behavioral/command/src/main.cc
@@ -3,9 +3,22 @@
 #include "src/light_bulb/include/light_bulb.h"
 #include "src/command/include/turn_on_light_command.h"
 
+struct Remote {
+	std::shared_ptr<Command> turn_on_;
+	void PressTurnOn() {
+		turn_on_->Execute();
+	}
+	void UndoTurnOn() {
+		turn_on_->Unexecute();
+	}
+};
+
 int main() {
 	auto bulb = std::make_shared<LightBulb>();
-	TurnOnLightCommand command(bulb);	
-	command.Execute();
-	command.Unexecute();
+	auto command = std::make_shared<TurnOnLightCommand>(bulb);	
+	Remote remote;
+	remote.turn_on_ = command;
+
+	remote.PressTurnOn();
+	remote.UndoTurnOn();
 }
